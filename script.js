e5a6d2719bc5f87476b82ca394a9b5b83e10f6dc
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-11-30 03:01:31 -0300

    js

---
+++ script.js
@@ -0,0 +1,77 @@
+// elementos
+const notesContainer = document.querySelector("#notes-container")
+
+const noteInput = document.querySelector("#note-content")
+
+const addNoteBtn = document.querySelector(".add-note")
+
+// Funções
+function showNotes(){
+    getNotes().forEach(note => {
+        const noteElement = createNote(note.id,note.content,note.fixed)
+
+        notesContainer.appendChild(noteElement)
+    });
+}
+
+
+function addNote(){
+
+    const notes = getNotes()
+
+
+    const notaObject = {
+        id: generateid,
+        content: noteInput.value,
+        fixed: false,
+    }
+
+
+   const noteElement= createNote(notaObject.id, notaObject.content)
+
+   notesContainer.appendChild(noteElement)
+
+   notes.push(notaObject)
+
+   saveNotes(notes)
+
+   noteInput.value = ""
+}
+
+function generateid(){
+    return Math.floor(Math.random() * 5000)
+}
+
+function createNote(id, content,fixed){
+
+    const element = document.createElement("div")
+
+    element.classList.add("note")
+    const textarea = document.createElement("textarea")
+    textarea.value = content
+
+    textarea.placeholder = "Adicione algum texto ..."
+
+    element.appendChild(textarea)
+
+    return element;
+}
+
+// Local storage
+function getNotes(){
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]")
+
+    return notes
+}
+
+
+function saveNotes(notes){
+    localStorage.setItem("notes", JSON.stringify(notes))
+}
+
+
+// Eventos
+addNoteBtn.addEventListener("click", () => addNote())
+
+// inicialização
+showNotes()
